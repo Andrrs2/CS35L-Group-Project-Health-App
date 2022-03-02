de428b2a25dd77febdd3fcbc3e6fe1b781081c4c
jill <pantigjillian0@gmail.com>
2022-03-02 01:09:44 -0800

    fixing bus and updating App.css and App.js *Adding images needed for our website

--- healthapp/src/App.js
+++ healthapp/src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignUp from './components/pages/SignUp';
+import CalorieCounter from './components/pages/CalorieCounter';
+import ExerciseTimer from './components/pages/ExerciseTimer';
+import WeightTracker from './components/pages/WeightTracker';
+import SleepTracker from './components/pages/SleepTracker';
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-        <Switch>
-          <Route path="/" exact />
-        </Switch>
+        <Routes>
+          <Route path="/sleep_tracker" element={<SleepTracker/>}/>
+          <Route path="/calorie_counter" element={<CalorieCounter/>}/>
+          <Route path="/exercise_timer" element={<ExerciseTimer/>}/>
+          <Route path="/weight_tracker" element={<WeightTracker/>}/>
+          <Route path="/sign-up" element={<SignUp/>}/>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </Router>
     </>
   );

f7581afb6d061c91dba92c0182c9607f00dedf03
jill <pantigjillian0@gmail.com>
2022-03-02 00:21:05 -0800

    removed menubar.js and built Navbar.js which is a tweaked version of menubar.js -- contains the options or features of the webapp

---
+++ healthapp/src/components/Navbar.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from 'react';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
+
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            HLTH
+            <i class='fa-solid fa-heart-pulse' />
+          </Link>  
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link
+                to='/exercise_timer'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Exercise Timer
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/calorie_counter'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Calorie Counter
+              </Link>
+            </li>
+
+            <li className='nav-item'>
+              <Link
+                to='/calorie_counter'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Weight Tracker
+              </Link>
+            </li>
+
+            <li className='nav-item'>
+              <Link
+                to='/sleep_tracker'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Sleep Tracker
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to='/sign-up'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}
+              >
+                Sign In
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle='btn--outline'>SIGN IN</Button>}
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;

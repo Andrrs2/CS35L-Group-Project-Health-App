419f423b01c61c03b40376f61bece9fc329fa07d
jill <pantigjillian0@gmail.com>
2022-03-02 00:30:57 -0800

    added pages for features for the webapp

--- healthapp/src/pages/WeightTracker.js
+++ healthapp/src/pages/WeightTracker.js
@@ -1,12 +1,6 @@
 import React from 'react';
-const weightTracker = () =>{
-  return (
-    <div>
-      <h3>Weight-Tracker</h3>
-      <div>
-        <img src="https://www.weightwatchers.com/us/shop/assets-proxy/weight-watchers/image/upload/q_auto/v1/prod/en-us-ec/static-asset/74108356581_conair_346_scale_f_1200.jpg?auto=webp" alt ="weight"/>
-      </div>
-    </div>
-  );
+import '../../App.css';
+
+export default function WeightTracker() {
+  return <h1 className='weight_tracker'>WEIGHT TRACKER</h1>;
 }
-export default weightTracker;
--- healthapp/src/pages/exerciseTimer.js
+++ healthapp/src/pages/exerciseTimer.js
@@ -1,12 +1,34 @@
 import React from 'react';
-const exerciseTimer = () =>{
+import { Navigate } from 'react-router-dom';
+import {useState} from "react";
+import '../../App.css';
+import Simple from '../exercise_timer/Simple.js'
+import TodoList from '../todo/TodoList';
+import { Link } from 'react-router-dom';
+
+
+var finished = false
+
+const handleStart = () => {
+    finished = true
+    console.log(finished)
+  };
+
+export default function ExerciseTimer() {
+    const closeMobileMenu = () => setClick(false);
+    const [click, setClick] = useState(false);
   return (
-    <div>
-      <h3>Exercise-Timer</h3>
-      <div>
-        <img src="https://www.poynter.org/wp-content/uploads/2003/06/shutterstock_549656056-1500x1000.jpg" alt = "timer"/>
-      </div>
-    </div>
+    <>
+      {/* <Simple /> */}
+      <TodoList />
+      <Link
+                to='/sign-up'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}
+              >
+                Sign In
+              </Link>
+              <center><button className='centerized'> Start Workout </button></center>
+    </>
   );
 }
-export default exerciseTimer;

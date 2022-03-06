e2138e4675624fcbd854df78fd3ac3e42b3a6d1e
Andres Blanco <blancoandres45@g.ucla.edu>
2022-03-06 15:54:03 -0800

    Template for weight tracker, used code for calorie counter and will be adding charts, kept styling so they'll be matching with other pages

--- healthapp/src/components/pages/WeightTracker.js
+++ healthapp/src/components/pages/WeightTracker.js
@@ -1,6 +1,63 @@
-import React from 'react';
-import '../../App.css';
+import { calculateNewValue } from '@testing-library/user-event/dist/utils';
+import React, { useRef, useState } from 'react';
+import { Form, } from "react-bootstrap";
+import { CheckBox, Text, StyleSheet, View} from "react-native";
+import weightTracker from './WeightTracker';
 
-export default function WeightTracker() {
-  return <h1 className='weight_tracker'>WEIGHT TRACKER</h1>;
-}
+export default class Kcal extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state = {
+      weight: 0,
+      gender: "male",
+      isSelected: false,
+    };
+  }
+  render()
+  {
+    return (
+      <View style = {styles.container}>
+      <h3>Weight Tracker:</h3>
+        <View style = {styles.checkboxContainer}>
+          <CheckBox style = {styles.checkbox}
+            value = {this.state.isSelected}
+            onValueChange={() => this.setState({isSelected: !this.state.isSelected})}
+          />
+          <Text>
+            Metric System
+          </Text>
+          
+        </View>
+        <View style={styles.container}>
+        <Form>
+          <Form.Group id="weight">
+            <Form.Label>Weight ({this.state.isSelected ? "kg" : "lb"})</Form.Label>
+            <Form.Control type="weight" value={this.state.weight} onChange={e => this.setState({weight: e.target.value })} required />
+          </Form.Group> 
+        </Form>
+        </View>
+        </View>
+      )
+    }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    marginBottom: 20,
+    marginTop: 10,
+  },
+  checkboxContainer: {
+    flexDirection: "row",
+    marginBottom: 20,
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  checkbox: {
+    alignSelf: "center",
+    marginHorizontal: 20,
+  },
+});

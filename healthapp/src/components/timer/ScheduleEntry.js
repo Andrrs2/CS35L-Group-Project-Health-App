4aee62d2f30e4c5ff23268da1eaf32ee2a7f94e8
jill <pantigjillian0@gmail.com>
2022-03-06 01:26:27 -0800

    Adding elements of Timer_Test to give rise to Exercise Timer

---
+++ healthapp/src/components/timer/ScheduleEntry.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddIcon from '@material-ui/icons/Add';
+import ListItem from '@material-ui/core/ListItem';
+import PeriodInput from './PeriodInput.js';
+import TextField from '@material-ui/core/TextField';
+
+export default function ScheduleEntry({index, isFirst, isLast, editable, entry, upFunc, downFunc, delFunc, addFunc, editFunc}) {
+    return (
+        <ListItem divider>
+            <Grid container direction="row" justify="space-between" alignItems="center">
+                <Grid container item xs={2} sm={2} md={2} lg={2} xl={2} justify="flex-start" alignItems="center">
+                    <TextField
+                        disabled={!editable}
+                        margin="dense"
+                        value={entry.name}
+                        onChange={(e) => {editFunc(index, "name", e.target.value)}}
+                    />
+                </Grid>
+                <Grid container item xs={6} sm={6} md={6} lg={6} xl={6} justify="center" alignItems="center">
+                    <PeriodInput disabled={!editable} min={entry.min} sec={entry.sec} editFunc={editFunc} index={index}></PeriodInput>
+                </Grid>
+                <Grid container item xs={4} sm={4} md={4} lg={4} xl={4} justify="flex-end" alignItems="center" direction="row">
+                    <IconButton item color="primary" disabled={isFirst || !editable} disableElevation onClick={() => {upFunc(index);}}>
+                        <KeyboardArrowUpIcon></KeyboardArrowUpIcon>
+                    </IconButton>
+                    <IconButton item color="primary" disabled={isLast || !editable} disableElevation onClick={() => {downFunc(index);}}>
+                        <KeyboardArrowDownIcon></KeyboardArrowDownIcon>
+                    </IconButton>
+                    <IconButton item color="primary" disabled={!editable} disableElevation onClick={() => {addFunc(index);}}>
+                        <AddIcon></AddIcon>
+                    </IconButton>
+                    <IconButton item color="primary" disabled={!editable} disableElevation onClick={() => {delFunc(index);}}>
+                        <DeleteIcon></DeleteIcon>
+                    </IconButton>
+                </Grid>
+            </Grid>
+        </ListItem>
+    );
+}
---
+++ healthapp/src/components/timer/TimerComponent.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Box from "@material-ui/core/Box";
+
+const useStyles = theme => ({
+    timer: {
+        backgroundColor: theme.palette.background.paper,
+        flexGrow: 0.4,
+    },
+    timerActual: {
+        width: "50%",
+        flexGrow: 0.6
+    },
+    timeText: {
+        color: "#005a2c",
+        userSelect: "none" // so that you cannot highlight the time, did so for personal aesthetics
+    },
+    timerContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: "75%",
+        borderColor: "#005810"
+    },
+});
+
+class TimerComponent extends React.Component {
+    render() {
+        const { classes } = this.props;
+        const { start, stop, reset, current, next, min, sec } = this.props.data;
+        return (
+            <Grid container item direction="column" alignItems="center" justify="space-evenly" className={classes.timer}>
+                <Box item display="flex" flexDirection="column" alignItems="center" justifyContent="center" >
+                <Grid container direction="column" alignItems="center">
+                    <Typography item variant="h2">{current}</Typography>
+                    <Typography item variant="h5">Next: {next}</Typography>
+                </Grid>
+                    <Button onClick={() => this.props.func(true)} variant="outlined" size="small">Skip</Button>
+                </Box>
+                <Box item display="flex" flexDirection="column" alignItems="center" justifyContent="center" >
+                    <Box border={5} className={classes.timerContainer} display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+                        <Grid container direction="row" justify="center">
+                            <Typography item variant="h3" className={classes.timeText}>{min}</Typography>
+                            <Typography item variant="h3" className={classes.timeText}>:</Typography>
+                            <Typography item variant="h3" className={classes.timeText}>{sec}</Typography>
+                        </Grid>
+                     </Box>
+                </Box>
+                <Box item display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+                    <ButtonGroup item color="primary" size="large" variant="contained">
+                        <Button onClick={stop}>Stop</Button>
+                        <Button onClick={start}>Start</Button>
+                        <Button onClick={reset}>Reset</Button>
+                    </ButtonGroup>
+                </Box>
+            </Grid>
+        );
+    }
+}
+
+export default withStyles(useStyles)(TimerComponent);

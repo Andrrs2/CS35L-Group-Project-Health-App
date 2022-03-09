708c6d6c858d3525cb1cb0ffa8121c5894ba47f6
Andres Blanco <blancoandres45@g.ucla.edu>
2022-03-09 14:03:47 -0800

    Fix for local storage not working, and graph is updated with a scatter graph that updates the domain if a weight greater than 250 is inputted

--- healthapp/src/components/pages/WeightTracker.js
+++ healthapp/src/components/pages/WeightTracker.js
@@ -1,31 +1,38 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState} from 'react';
 import { Form, } from "react-bootstrap";
 import "../../App.css";
 import { CheckBox, Text, StyleSheet, View} from "react-native";
 import Paper from '@mui/material/Paper';
 import {ArgumentAxis, ValueAxis, Chart, LineSeries, SplineSeries} from '@devexpress/dx-react-chart-material-ui';
+import { ScatterSeries, ValueScale} from '@devexpress/dx-react-chart';
+
+
 export default class weightTracker extends React.Component{
   constructor(props){
     super(props)
     this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
+      domain: 250,
       weight: 0,
-      track: localStorage.getItem('track') === null ? 1 : JSON.parse(localStorage.getItem('track')),
-      DataArray: localStorage.getItem('DataArray') === null ? [{argument: 0, value: 0}] : JSON.parse(localStorage.getItem('DataArray')),
+      track: JSON.parse(window.sessionStorage.getItem('track')) === null ? 1 : JSON.parse(window.sessionStorage.getItem('track')),
+      DataArray: JSON.parse(window.sessionStorage.getItem('DataArray') === null) ? [{argument: 0, value: 0}] : JSON.parse(window.sessionStorage.getItem('DataArray')),
     }
   }
+
   handleSubmit(event) {
+    if (this.state.weight > this.state.domain)
+    {
+      this.setState({domain: this.state.weight})
+    }
     this.setState({track: this.state.track + 1})
     let DataArray = [...this.state.DataArray];
-    DataArray[0] = {argument: 0, value: 0}
     DataArray[this.state.track] = {argument: this.state.track, value: this.state.weight};
     this.setState({ DataArray });
-    localStorage.setItem('DataArray',  JSON.stringify(DataArray));
-    localStorage.setItem('track', JSON.stringify(this.state.track));
-    alert('A weight was submitted: ' + this.state.weight);
+    window.sessionStorage.setItem('track',  JSON.stringify(this.state.track));
+    window.sessionStorage.setItem('DataArray',  JSON.stringify(DataArray));
+    alert('Submitted weight: ' + this.state.weight);
     event.preventDefault();
   }
-  
   render()
   {
     return (
@@ -39,7 +46,7 @@ export default class weightTracker extends React.Component{
             <input className="submit_form" type="submit" value="Submit" />
           </Form.Group>
         <View color="#01793b" style = {styles.checkbox}>
-        <form onSubmit={localStorage.clear()}>
+        <form onSubmit={sessionStorage.clear()}>
          <input className="input_form" type="submit" value="Clear Storage" />
           </form>  
           </View>
@@ -51,9 +58,10 @@ export default class weightTracker extends React.Component{
           <Chart
              data = {this.state.DataArray}
           >
+            <ValueScale modifyDomain={() => [0, this.state.domain]} />
             <ArgumentAxis />
             <ValueAxis />
-            <LineSeries valueField="value" argumentField="argument" color='BLUE' /> 
+            <ScatterSeries valueField="value" argumentField="argument" color='BLUE' /> 
             <SplineSeries
             valueField="splineValue"
             argumentField="argument"
@@ -65,6 +73,7 @@ export default class weightTracker extends React.Component{
       )
     }
 };
+
 
 const styles = StyleSheet.create({
   container: {

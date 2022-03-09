b6281ad5eeec86815b93e6261b0694eb4efea0ea
Andres Blanco <blancoandres45@g.ucla.edu>
2022-03-09 00:06:15 -0800

    Saves user input to local storage and gives them an option to clear the storage if they want a new graph

--- healthapp/src/components/pages/WeightTracker.js
+++ healthapp/src/components/pages/WeightTracker.js
@@ -3,42 +3,47 @@ import { Form, } from "react-bootstrap";
 import { CheckBox, Text, StyleSheet, View} from "react-native";
 import Paper from '@mui/material/Paper';
 import {ArgumentAxis, ValueAxis, Chart, LineSeries, SplineSeries} from '@devexpress/dx-react-chart-material-ui';
-
 export default class weightTracker extends React.Component{
   constructor(props){
     super(props)
     this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
       weight: 0,
-      track: 1,
-      DataArray: [{argument: 0, value: 0}]
+      track: localStorage.getItem('track') === null ? 1 : JSON.parse(localStorage.getItem('track')),
+      DataArray: localStorage.getItem('DataArray') === null ? [{argument: 0, value: 0}] : JSON.parse(localStorage.getItem('DataArray')),
     }
   }
-
   handleSubmit(event) {
     this.setState({track: this.state.track + 1})
     let DataArray = [...this.state.DataArray];
     DataArray[0] = {argument: 0, value: 0}
     DataArray[this.state.track] = {argument: this.state.track, value: this.state.weight};
     this.setState({ DataArray });
+    localStorage.setItem('DataArray',  JSON.stringify(DataArray));
+    localStorage.setItem('track', JSON.stringify(this.state.track));
     alert('A weight was submitted: ' + this.state.weight);
     event.preventDefault();
   }
-
+  
   render()
   {
     return (
       <View style = {styles.container}>
       <h3>Weight Tracker:</h3>
-        <View style = {styles.checkboxContainer}>
+      <View style = {styles.checkboxContainer}>
         <View style={styles.container}>
         <Form onSubmit= {this.handleSubmit}>
           <Form.Group id="weight">
             <Form.Label>Weight (lb)</Form.Label>
-            <Form.Control type="weight" value={this.state.weight} onChange={e => this.setState({weight: e.target.value})} required />
+            <Form.Control type="weight" value={this.state.weight}  onChange={e => this.setState({weight: e.target.value})} required />
             <input type="submit" value="Submit" />
           </Form.Group>
-        </Form>
+        <View style = {styles.checkbox}>
+        <form onSubmit={localStorage.clear()}>
+         <input type="submit" value="Clear Storage" />
+          </form>  
+          </View>
+        </Form> 
         </View>
         </View>
         <View style={styles.picker}>
